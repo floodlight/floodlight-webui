6d0f0a1c9804f05f2a5b98b655e6ee2a1ba41aac
hknakcay <hknakcay@gmail.com>
2016-02-08 11:11:49 +0200

    SDN Web API UI v1.0

---
+++ js/querystringparser.js
@@ -0,0 +1,8 @@
+﻿function getQueryParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var controllerIp = getQueryParameterByName("cip");
